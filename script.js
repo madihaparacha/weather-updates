9ad3839f3aec18d98a638431e0bab14e52f14206
madihaparacha <38943126+madihaparacha@users.noreply.github.com>
2018-09-01 10:56:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -109,6 +109,131 @@ function getWeather() {
                     else if(forecast1 == "50d" || forecast1 == "50n"){
                         document.querySelector(".icons-mon").src = "https://www.dovora.com/resources/weather-icons/showcase/simplistic_showcase/mist.png";
                     }
+            
+        // tuesday
+        let forecast2 = data.list[8].weather[0].icon;
+        if(forecast2 == "01d" || forecast2 == "01n"){
+            document.querySelector(".icons-tue").src = "https://png.icons8.com/ios/50/ffffff/sun-filled.png";
+        }
+        else if(forecast2 == "02d" || forecast2 == "02n"){
+            document.querySelector(".icons-tue").src = "https://png.icons8.com/wired/50/ffffff/clouds.png";
+        }
+        else if(forecast2 == "03d" || forecast2 == "03n"){
+            document.querySelector(".icons-tue").src = "https://png.icons8.com/ios/50/ffffff/cloudflare.png";
+        }
+        else if(forecast2 == "04d" || forecast2 == "04n"){
+            document.querySelector(".icons-tue").src = "https://png.icons8.com/ios/50/ffffff/cloudflare.png";
+        }
+        else if(forecast2 == "09d" || forecast2 == "09n"){
+            document.querySelector(".icons-tue").src = "https://png.icons8.com/wired/50/ffffff/rain.png";
+        }
+        else if(forecast2 == "10d" || forecast2 == "10n"){
+            document.querySelector(".icons-tue").src = "https://png.icons8.com/ios/50/ffffff/torrential-rain.png";
+        }
+        else if(forecast2 == "11d" || forecast2 == "11n"){
+            document.querySelector(".icons-tue").src = "https://png.icons8.com/wired/50/ffffff/storm.png";
+        }
+        else if(forecast2 == "13d" || forecast2 == "13n"){
+            document.querySelector(".icons-tue").src = "https://png.icons8.com/ios/50/ffffff/winter.png";
+        }
+        else if(forecast2 == "50d" || forecast2 == "50n"){
+            document.querySelector(".icons-tue").src = "https://www.dovora.com/resources/weather-icons/showcase/simplistic_showcase/mist.png";
+        }
+
+
+
+
+        // wednesday
+        let forecast3 = data.list[8].weather[0].icon;
+        if (forecast3 == "01d" || forecast3 == "01n"){
+            document.querySelector(".icons-wed").src = "https://png.icons8.com/ios/50/ffffff/sun-filled.png";
+        }
+        else if (forecast3 == "02d" || forecast3 == "02n"){
+            document.querySelector(".icons-wed").src = "https://png.icons8.com/wired/50/ffffff/clouds.png";
+        }
+        else if (forecast3 == "03d" || forecast3 == "03n"){
+            document.querySelector(".icons-wed").src = "https://png.icons8.com/ios/50/ffffff/cloudflare.png";
+        }
+        else if (forecast3 == "04d" || forecast3 == "04n"){
+            document.querySelector(".icons-wed").src = "https://png.icons8.com/ios/50/ffffff/cloudflare.png";
+        }
+        else if (forecast3 == "09d" || forecast3 == "09n"){
+            document.querySelector(".icons-wed").src = "https://png.icons8.com/wired/50/ffffff/rain.png";
+        }
+        else if (forecast3 == "10d" || forecast3 == "10n"){
+            document.querySelector(".icons-wed").src = "https://png.icons8.com/ios/50/ffffff/torrential-rain.png";
+        }
+        else if (forecast3 == "11d" || forecast3 == "11n"){
+            document.querySelector(".icons-wed").src = "https://png.icons8.com/wired/50/ffffff/storm.png";
+        }
+        else if (forecast3 == "13d" || forecast3 == "13n"){
+            document.querySelector(".icons-wed").src = "https://png.icons8.com/ios/50/ffffff/winter.png";
+        }
+        else if (forecast3 == "50d" || forecast3 == "50n"){
+            document.querySelector(".icons-wed").src = "https://www.dovora.com/resources/weather-icons/showcase/simplistic_showcase/mist.png";
+        }
+
+
+
+        // thursday
+        let forecast4 = data.list[8].weather[0].icon;
+        if (forecast4 == "01d" || forecast4 == "01n"){
+                        document.querySelector(".icons-thurs").src = "https://png.icons8.com/ios/50/ffffff/sun-filled.png";
+                    }
+                    else if(forecast4 == "02d" || forecast4 == "02n"){
+                        document.querySelector(".icons-thurs").src = "https://png.icons8.com/wired/50/ffffff/clouds.png";
+                    }
+                    else if(forecast4 == "03d" || forecast4 == "03n"){
+                        document.querySelector(".icons-thurs").src = "https://png.icons8.com/ios/50/ffffff/cloudflare.png";
+                    }
+                    else if(forecast4 == "04d" || forecast4 == "04n"){
+                        document.querySelector(".icons-thurs").src = "https://png.icons8.com/ios/50/ffffff/cloudflare.png";
+                    }
+                    else if(forecast4 == "09d" || forecast4 == "09n"){
+                        document.querySelector(".icons-thurs").src = "https://png.icons8.com/wired/50/ffffff/rain.png";
+                    }
+                    else if(forecast4 == "10d" || forecast4 == "10n"){
+                        document.querySelector(".icons-thurs").src = "https://png.icons8.com/ios/50/ffffff/torrential-rain.png";
+                    }
+                    else if(forecast4 == "11d" || forecast4 == "11n"){
+                        document.querySelector(".icons-thurs").src = "https://png.icons8.com/wired/50/ffffff/storm.png";
+                    }
+                    else if(forecast4 == "13d" || forecast1 == "13n"){
+                        document.querySelector(".icons-thurs").src = "https://png.icons8.com/ios/50/ffffff/winter.png";
+                    }
+                    else if(forecast4 == "50d" || forecast4 == "50n"){
+                        document.querySelector(".icons-thurs").src = "https://www.dovora.com/resources/weather-icons/showcase/simplistic_showcase/mist.png";
+                    }
+
+                //   friday
+                    let forecast5 = data.list[8].weather[0].icon;
+                    if(forecast5 == "01d" || forecast5 == "01n"){
+                        document.querySelector(".icons-fri").src = "https://png.icons8.com/ios/50/ffffff/sun-filled.png";
+                    }
+                    else if(forecast5 == "02d" || forecast5 == "02n"){
+                        document.querySelector(".icons-fri").src = "https://png.icons8.com/wired/50/ffffff/clouds.png";
+                    }
+                    else if(forecast5 == "03d" || forecast5 == "03n"){
+                        document.querySelector(".icons-fri").src = "https://png.icons8.com/ios/50/ffffff/cloudflare.png";
+                    }
+                    else if(forecast5 == "04d" || forecast5 == "04n"){
+                        document.querySelector(".icons-fri").src = "https://png.icons8.com/ios/50/ffffff/cloudflare.png";
+                    }
+                    else if(forecast5 == "09d" || forecast5 == "09n"){
+                        document.querySelector(".icons-fri").src = "https://png.icons8.com/wired/50/ffffff/rain.png";
+                    }
+                    else if(forecast5 == "10d" || forecast5 == "10n"){
+                        document.querySelector(".icons-fri").src = "https://png.icons8.com/ios/50/ffffff/torrential-rain.png";
+                    }
+                    else if(forecast5 == "11d" || forecast5 == "11n"){
+                        document.querySelector(".icons-fri").src = "https://png.icons8.com/wired/50/ffffff/storm.png";
+                    }
+                    else if(forecast5 == "13d" || forecast5 == "13n"){
+                        document.querySelector(".icons-fri").src = "https://png.icons8.com/ios/50/ffffff/winter.png";
+                    }
+                    else if(forecast5 == "50d" || forecast5 == "50n"){
+                        document.querySelector(".icons-fri").src = "https://www.dovora.com/resources/weather-icons/showcase/simplistic_showcase/mist.png";
+                    }
             document.querySelector(".exp-temp-tues").innerHTML = Math.round(data.list[5].main.temp) + degree + "/" + Math.round(data.list[6].main.temp) + degree;
             document.querySelector(".exp-temp-wed").innerHTML = Math.round(data.list[13].main.temp) + degree + "/" + Math.round(data.list[14].main.temp) + degree;
             document.querySelector(".exp-temp-thurs").innerHTML = Math.round(data.list[21].main.temp) + degree + "/" + Math.round(data.list[22].main.temp) + degree;
